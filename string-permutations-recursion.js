bf34d602e1a6f785bbfbf4cd3fa68b63387a2420
AChandel01071994 <31288986+AChandel01071994@users.noreply.github.com>
2019-07-16 19:28:38 +0530

    string permutations using recursion

---
+++ string-permutations-recursion.js
@@ -0,0 +1,29 @@
+// permutations of string - n!
+{
+     function permutations(str, count, result, level, allResult){
+         // if recursion level has reached same as string length
+         if(level === str.length){
+            allResult.push([...result]);
+         } else {
+             // loop through all characters
+          for(let i = 0; i < str.length; i++){
+              if(count[i] === 1) {
+                  result[level] = str[i];
+                  count[i] = 0;
+                  level++;
+                  // recursive call
+                  permutations(str, count, result, level, allResult);
+                  level--;
+                  count[i] = 1;
+              }
+            
+          }
+         }
+     }
+    
+    let str = 'abc', result = [], allResult = [], level = 0;
+    // array containing 1s with same length as str
+    let countArr = Array.from({length : str.length}, () => 1 );
+    permutations(str, [1,1,1], result, level, allResult);
+    console.log(allResult);
+}
